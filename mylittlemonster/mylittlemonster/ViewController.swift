3c35ef90780c18ec0d310e64bff7c0a925793374
Rea Won Kim <ngoccuong11789@gmail.com>
2016-02-21 16:58:17 +0700

    Up

---
+++ mylittlemonster/mylittlemonster/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  mylittlemonster
+//
+//  Created by Rea Won Kim on 2/21/16.
+//  Copyright © 2016 Rea Won Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var imgArray = [UIImage]()
+        for var x = 1; x <= 4; x++ {
+            let img = UIImage(named: "idle\(x).png")
+            imgArray.append(img!)
+        }
+        
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
+    }
+    
+
+
+
+
+
+}
+
